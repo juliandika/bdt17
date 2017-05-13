ccf23b43716ce97c68b0cd042d76b4c922befe02
juliandika <juuliandika@gmail.com>
2017-05-13 15:17:41 +0800

    add calculator

--- calculator.js
+++ calculator.js
@@ -2,22 +2,43 @@ var calculator = {
 	
 	tambah : function(a,b) {
 
-		return
-	}
+		return a + b;
+	},
 
-	kurang : '',
-	kali : '',
+	kurang : function(a,b) {
+		return a - b;
+	},
+
+	kali : function(a,b) {
+		return a * b;
+	},
+
+	bagi : function(a,b) {
+		return a/b;
+	},
+
 	samadengan : function(a,b) {
 
-		if() {
+		if(a > b) {
 
-			return a + 'Lebih besar dari ' + b;
+			return a + ' lebih besar dari ' + b;
 
-		} elseif() {
+		}
+		if(a < b ) {
 
+			return a + ' lebih kecil dari ' + b;
 		} else {
 
+			return a + ' samadengan '+ b;
 		}
 	}
 
 }
+
+
+
+console.log(calculator.tambah(8,9));
+console.log(calculator.kurang(8,9));
+console.log(calculator.kali(8,9));
+console.log(calculator.bagi(8,9));
+console.log(calculator.samadengan(8,9));
